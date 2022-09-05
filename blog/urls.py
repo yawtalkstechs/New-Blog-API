05dab5b7cbe24b1dc19df731fb7117b5d5d6e990
yawtalkstechs <yawtalkstechs@gmail.com>
2022-09-05 18:22:34 +0000

    Email form added and sharing a post added

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import post_detail, post_list
+from .views import post_detail, post_list, PostListView, post_share
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
+    # path('', post_list, name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('<int:post_id>/share/', post_share, name='post_share'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
      post_detail, name='post_detail'),
 ]
