a58e802e7dd76d3f2811d1f312847e3649f26939
yawtalkstechs <yawtalkstechs@gmail.com>
2022-09-12 16:35:36 +0000

    RSS and Atom Feed Added

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import post_detail, post_list, PostListView, post_share, post_comment
+from .feeds import LatestPostsFeed, AtomPostsFeed
 
 app_name = "blog"
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('atom/', AtomPostsFeed(), name='post_atom'),
     # path("", PostListView.as_view(), name="post_list"),
     path("<int:post_id>/share/", post_share, name="post_share"),
     path("tag/<slug:tag_slug>/", post_list, name="post_list_by_tag"),
